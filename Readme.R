4886bb4e288f6476a4296e84656c8ca63ff177cc
Péter Kalicz <kaliczp@gmail.com>
2020-12-27 11:01:48 +0100

    First step to substract time difference
    A second sapply() substracts the noted time difference with the initial
    processdiff() function and finally composed a data.frame.
    
    Intitial version of the precessdiff() function substract only the first
    number from the text row without date and time of data readout.

--- Readme.R
+++ Readme.R
@@ -5,12 +5,20 @@ preparenote <- function(file) {
     processreadoutdate  <- function(notetext){
         paste(notetext[1], notetext[2])
     }
+    ## Substract time difference from text
+    processdiff <- function(notetext){
+        textwithdiff <- notetext[3:length(notetext)]
+        numbersindex <- grep("[0-9]", textwithdiff)
+        textwithdiff[numbersindex[1]]
+    }
     ## Read in file as character
     rawnote <- scan(file, character(), sep="\n")
     ## Split file at spaces
     splittednote <- strsplit(rawnote, " ")
     ## Process resulted list
-    sapply(splittednote, processreadoutdate)
+    datetime <- sapply(splittednote, processreadoutdate)
+    timediff <- sapply(splittednote, processdiff)
+    data.frame(datetime, timediff = as.numeric(timediff))
 }
 
 preparenote(ttfiles[1])
