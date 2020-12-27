2f85116ca15e7a33ea52e2e2fc7f0d3589647062
Péter Kalicz <kaliczp@gmail.com>
2020-12-27 12:29:13 +0100

    Finer processing of time difference
    Now minutes an seconds are treated based on their position.
    The result is returned as seconds.

--- Readme.R
+++ Readme.R
@@ -9,7 +9,20 @@ preparenote <- function(file) {
     processdiff <- function(notetext){
         textwithdiff <- notetext[3:length(notetext)]
         numbersindex <- grep("[0-9]", textwithdiff)
-        textwithdiff[numbersindex[1]]
+        ## Process the numbers
+        if(length(numbersindex) == 1){
+            ## In the case one number available, suppose it seconds
+            secdiff <- textwithdiff[numbersindex]
+        } else {
+            if(length(numbersindex) == 2){
+                ## If two numbers available maybe mins and seconds
+                minsinsec <- as.numeric(textwithdiff[numbersindex[1]]) * 60
+                secdiff <- minsinsec + as.numeric(textwithdiff[numbersindex[2]])
+            } else {
+                warning("More than two numbers are in the text!")
+            }
+        }
+        secdiff
     }
     ## Read in file as character
     rawnote <- scan(file, character(), sep="\n")
