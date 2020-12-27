d23bb0c1ddaa8523188805c8e76c2e9595b4af45
Péter Kalicz <kaliczp@gmail.com>
2020-12-27 10:38:59 +0100

    New subfunction to date and time substraction
    Now in preparenote() a separate function makes date processing.  It
    substract the first two words of the row which is usually date and
    time.

--- Readme.R
+++ Readme.R
@@ -1,12 +1,16 @@
 ttfiles <- dir(patt="^Boreas20[12][0-9].txt")
 
 preparenote <- function(file) {
+    ## Function to substract readout date from text note
+    processreadoutdate  <- function(notetext){
+        paste(notetext[1], notetext[2])
+    }
     ## Read in file as character
     rawnote <- scan(file, character(), sep="\n")
     ## Split file at spaces
     splittednote <- strsplit(rawnote, " ")
     ## Process resulted list
-    sapply(splittednote, function(x){x[1]})
+    sapply(splittednote, processreadoutdate)
 }
 
 preparenote(ttfiles[1])
